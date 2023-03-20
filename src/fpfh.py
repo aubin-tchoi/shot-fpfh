d99e4551e81df230c3ad38ba26127f75d2e8389e
aubin-tchoi <aubin.tchoi@gmail.com>
2023-03-20 15:16:08 +0100

    FPFH part completed

--- src/fpfh.py
+++ src/fpfh.py
@@ -19,11 +19,12 @@ def compute_fpfh_descriptor(
     normals: np.ndarray,
     radius: float,
     k: int,
+    n_bins: int,
 ) -> np.ndarray:
     kdtree = KDTree(cloud_points)
 
     neighborhoods = kdtree.query_radius(query_points, radius)
-    spfh = np.zeros((query_points.shape[0], 3))
+    spfh = np.zeros((query_points.shape[0], n_bins, n_bins, n_bins))
     for i, point in tqdm(
         enumerate(query_points), desc="SPFH", total=query_points.shape[0]
     ):
@@ -34,18 +35,26 @@ def compute_fpfh_descriptor(
         u = normals[i]
         v = np.cross(u, centered_neighbors / distances[:, None])
         w = np.cross(u, v)
-        alpha = v @ neighbors_normals
-        phi = u @ centered_neighbors / distances[:, None]
-        theta = np.arctan2(w @ neighbors_normals, u @ neighbors_normals)
-        # TODO: add the bin accumulation part
-        spfh[i] = [alpha, phi, theta]
+        alpha = np.einsum("ij,ij->i", v, neighbors_normals)
+        phi = (centered_neighbors / distances[:, None]).dot(u)
+        theta = np.arctan2(
+            np.einsum("ij,ij->i", neighbors_normals, w), neighbors_normals.dot(u)
+        )
+        spfh[i, :, :, :] = (
+            np.histogramdd(
+                np.vstack((alpha, phi, theta)).T,
+                bins=n_bins,
+                range=[(-1, 1), (-1, 1), (-np.pi / 2, np.pi / 2)],
+            )[0]
+            / neighborhoods[i].shape[0]
+        )
 
     neighborhoods = kdtree.query(query_points, k, return_distance=False)
-    fpfh = np.zeros((query_points.shape[0], 3))
+    fpfh = np.zeros((query_points.shape[0], n_bins, n_bins, n_bins))
     for i, point in tqdm(
         enumerate(query_points), desc="FPFH", total=query_points.shape[0]
     ):
         distances = np.linalg.norm(cloud_points[neighborhoods[i]] - point, axis=1)
-        fpfh[i] = spfh[i] + (spfh[neighborhoods[i]] / distances) / k
+        fpfh[i] = spfh[i] + (spfh[neighborhoods[i]] / distances[:, None, None, None]).sum() / k
 
     return fpfh
