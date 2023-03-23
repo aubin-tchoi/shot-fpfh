205edd0b8737fa268cd7c02d233e61b953733336
aubin-tchoi <aubin.tchoi@gmail.com>
2023-03-23 10:20:25 +0100

    ICP runs can now be stopped preemptively by user, small optimization

--- src/icp.py
+++ src/icp.py
@@ -8,15 +8,15 @@ import numpy as np
 from sklearn.neighbors import KDTree
 from tqdm import trange
 
-from .rigid_transform import best_rigid_transform, compute_rigid_transform_error
+from .rigid_transform import best_rigid_transform
 
 
 def icp_point_to_point(
     data: np.ndarray,
     ref: np.ndarray,
-    max_iter: int = 500,
-    rms_threshold: float = 0.1,
-    sampling_limit: int = 10,
+    max_iter: int = 10000,
+    rms_threshold: float = 0.01,
+    sampling_limit: int = 100,
 ) -> Tuple[np.ndarray, float, bool]:
     """
     Iterative closest point algorithm with a point to point strategy.
@@ -36,22 +36,31 @@ def icp_point_to_point(
     """
     data_aligned = np.copy(data)
     kdtree = KDTree(ref)
+    sampling_limit = min(sampling_limit, data.shape[0])
 
     rms = 0.0
 
     for _ in trange(max_iter, desc="ICP", delay=1):
-        indexes = np.random.choice(data.shape[0], sampling_limit, replace=False)
-        data_aligned_subset = data_aligned[indexes]
-        neighbors = kdtree.query(data_aligned_subset, return_distance=False).squeeze()
-        rms, data_aligned = compute_rigid_transform_error(
-            data,
-            ref,
-            *best_rigid_transform(
+        # this loop can be stopped preemptively by a CTRL+C
+        try:
+            indexes = np.random.choice(data.shape[0], sampling_limit, replace=False)
+            data_aligned_subset = data_aligned[indexes]
+            neighbors = kdtree.query(data_aligned_subset, return_distance=False).squeeze()
+            rotation, translation = best_rigid_transform(
                 data_aligned_subset,
                 ref[neighbors],
-            ),
-        )
-        if rms < rms_threshold:
+            )
+            rms = np.sqrt(
+                np.sum(
+                    (data_aligned_subset - ref[neighbors]) ** 2,
+                    axis=0,
+                ).mean()
+            )
+            data_aligned = data_aligned.dot(rotation.T) + translation
+            if rms < rms_threshold:
+                break
+        except KeyboardInterrupt:
+            print("ICP interrupted by user.")
             break
 
     return data_aligned, rms, rms < rms_threshold
