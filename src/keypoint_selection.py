02205fb5d121b2b3802629351294e526187d0948
aubin-tchoi <aubin.tchoi@gmail.com>
2023-05-19 12:17:01 +0200

    typing updated

--- src/keypoint_selection.py
+++ src/keypoint_selection.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import numpy as np
 from sklearn.neighbors import KDTree
 
@@ -7,14 +9,16 @@ from .subsampling import grid_subsampling
 rng = np.random.default_rng(seed=1)
 
 
-def select_keypoints_iteratively(points: np.ndarray, radius: float) -> np.ndarray:
+def select_keypoints_iteratively(
+    points: np.ndarray[np.float64], radius: float
+) -> np.ndarray[np.int32]:
     """
     Selects a subset of the points to create a set of key points on which descriptors will be computed.
     Operates by selecting a point randomly, counting its spherical neighbors as visited,
     removing the visited points from the pool of candidate points and reiterating on it.
 
     Returns:
-        selected: array of the indices of the selected points.
+        selected: array containing the indices of the selected points.
     """
     selected = np.zeros(points.shape[0], dtype=bool)
     visited = np.zeros(points.shape[0], dtype=bool)
@@ -29,28 +33,30 @@ def select_keypoints_iteratively(points: np.ndarray, radius: float) -> np.ndarra
 
 
 def select_keypoints_subsampling(
-    points: np.ndarray, voxel_size: float
-) -> np.ndarray:
+    points: np.ndarray[np.float64], voxel_size: float
+) -> np.ndarray[np.int32]:
     """
     Selects a subset of the points to create a set of key points on which descriptors will be computed.
-    Operates by subsampling the point cloud and taking the points closest to the barycenter of each voxel is kept.
+    Operates by subsampling the point cloud and keeping the points closest to the barycenter of each voxel.
 
     Returns:
-        selected: array of the indices of the selected points.
+        selected key points: array containing the indices of the selected points.
     """
     return grid_subsampling(points, voxel_size)
 
 
 def select_keypoints_randomly(
-    points: np.ndarray, n_feature_points: int
-) -> np.ndarray:
+    points: np.ndarray[np.float64], n_feature_points: int
+) -> np.ndarray[np.int32]:
     """
     Selects a random subset of the points to create a set of key points on which descriptors will be computed.
     """
     return rng.choice(points, n_feature_points, replace=False, shuffle=False)
 
 
-def select_query_indices_randomly(n_points: int, n_feature_points: int) -> np.ndarray:
+def select_query_indices_randomly(
+    n_points: int, n_feature_points: int
+) -> np.ndarray[np.int32]:
     """
     Selects a random subset of the points to create a set of key points on which descriptors will be computed.
     """
